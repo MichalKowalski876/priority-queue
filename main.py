62656b052957c8b4203c0144f21d874b314aad2f
Gae <tom1s1okgry@gmail.com>
2025-05-12 21:53:36 +0200

    fixed fetch data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 from time import time
 
+
 def timer(func):
     def wrapper(*args, **kwargs):
         start_time = time()
@@ -8,27 +9,32 @@ def timer(func):
         end_time = time()
         print(f'{func.__name__} took {end_time - start_time:.6f} seconds')
         return result
+
     return wrapper
 
 
+@timer
 def fetch_data():
     try:
-        fh = open("queue_test.json")
-        queue = json.load(fh)
-        return queue
-
-    except FileExistsError:
+        with open("queue_test.json", 'r') as fh:
+            content = fh.read()
+            queue = json.loads(content)
+            return queue
+    except (FileNotFoundError, ValueError, json.JSONDecodeError):
         with open('queue_test.json', 'w') as fh:
             json.dump([], fh)
-            fetch_data()
+            return None
 
             # save_data([]) # change in final version
 
+
+@timer
 def save_data(data):
-    with open('queue.json', 'w') as f:
-        json.dump(data, f)
+    with open('queue.json', 'w') as fh:
+        json.dump(data, fh)
 
 
+@timer
 def queue_sort(data):
     swap = False
     data_length = len(data)
@@ -44,6 +50,7 @@ def queue_sort(data):
     save_data(data)
     display_data(data)
 
+
 @timer
 def display_data(data):
     print('Index no.     Priority     Value')
@@ -53,18 +60,22 @@ def display_data(data):
         print(str((value + 1)) + display_space + " " + str(data_dictionary["priority"]) + display_space + str(
             data_dictionary["value"]))
 
+
 def add_element(user_input):
     pass
+
 
 def delete_element(user_input):
     pass
 
+
 def display_elements(user_input):
     pass
+
 
 def main_menu():
     pass
 
 
 if __name__ == '__main__':
-    display_data(fetch_data())
+    fetch_data()
